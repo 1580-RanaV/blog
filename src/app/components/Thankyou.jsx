dc3546123c6f36242eb0888747e6a269e012516e
ranavgh18 <rviyyapu@gitam.in>
2024-11-17 10:21:15 +0530

    few changes

--- src/app/components/Thankyou.jsx
+++ src/app/components/Thankyou.jsx
@@ -7,8 +7,9 @@ const Thankyou = () => {
             <span>You've reached the end thank you for your time :)</span>
             <img src="/favicon-logo.png" alt="my-logo" className='w-full pb-0 mb-0' />
           </section>
-          <h6 className='text-center'>Made by V Ranadheer<br/>
-          All rights reserved, 2024.</h6>
+          <h6 className='text-center font-sans'>
+          All rights reserved, 2024 ©
+          vrana.fun and www.vrana.fun</h6>
     </div>
   )
 }
