b57f283ca3da2d50a9062296d6738f9e7e8421e2
ranavgh18 <rviyyapu@gitam.in>
2024-11-18 13:49:06 +0530

    count change

--- src/app/components/CurrentlyBuilding.jsx
+++ src/app/components/CurrentlyBuilding.jsx
@@ -13,7 +13,7 @@ const CurrentlyBuilding = () => {
               When you join our exclusive community, you're not just becoming a member; you're becoming part of a family that builds, supports, 
               and grows together. From academic concepts to real-world solutions, we've helped each other create magic through code!
               <br/><br/>
-              Speaking of magic, we're pretty proud of what our community has accomplished! With over 50+ sign ups across our products, 
+              Speaking of magic, we're pretty proud of what our community has accomplished! With over 100+ sign ups across our products, 
               we've built some pretty cool stuff. From making coding practice fun with SpeedCode, to automating blogging with AutoNabu,
               to outsmarting text detection with PureText, and even making legal documents a breeze with
               EasyLegal — we've got quite the portfolio going!
