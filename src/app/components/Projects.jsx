088e76c8a1f55d130b12a7ee874a6cf84e338632
ranavgh18 <rviyyapu@gitam.in>
2024-11-30 16:43:40 +0530

    ph added

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -14,8 +14,9 @@ const Projects = () => {
                 <img src="/puretext.png" alt="PureText" className="mb-2 border rounded-xl" />
                 <span className='font-mono mb-1.5 block'>
                 An application where users can convert regular text to plagiarism-free text with the help of a LLM
-                 — smooth, responsive, <i>blazing fast</i> and at the least 80% effective from tools like TurnItIn, Grammarly.
+                 — smooth, responsive, <i>blazing fast</i> and at the least 80% effective from tools like TurnItIn, Grammarly. It is now live on ProductHunt.
                 </span>
+                <div className='flex'>
                 <a 
                 className='text-blue-500 font-sans'
                 href='https://puretextbydatco.vercel.app'>
@@ -34,6 +35,26 @@ const Projects = () => {
                 </svg>
                 </span>
                 </a>
+
+                <a 
+                className='text-blue-500 font-sans'
+                href='https://www.producthunt.com/posts/puretext'>
+                 <span className='flex'>ProductHunt
+                 <svg 
+                  className="feather feather-arrow-up-right" 
+                  fill="none" 
+                  height="24" 
+                  stroke="currentColor" 
+                  viewBox="0 0 24 24" 
+                  width="24" 
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <line x1="7" x2="17" y1="17" y2="7"/>
+                  <polyline points="7 7 17 7 17 17"/>
+                </svg>
+                </span>
+                </a>
+                </div>
               </div>
             </div>
 
