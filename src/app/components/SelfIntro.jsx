69bcd7fc696201d9c5b96be2350e23794ceb0614
ranavgh18 <rviyyapu@gitam.in>
2025-09-28 14:07:37 +0530

    leading extra

--- src/app/components/SelfIntro.jsx
+++ src/app/components/SelfIntro.jsx
@@ -10,7 +10,7 @@ const SelfIntro = () => {
           <div className="h-1 w-full bg-gradient-to-r from-neutral-100 via-neutral-200 to-neutral-100 dark:from-neutral-900 dark:via-neutral-800 dark:to-neutral-900" />
 
           <div className="p-6 sm:p-8">
-            <p className="text-base text-center sm:text-lg leading-8 text-neutral-800 dark:text-neutral-200 font-mono">
+            <p className="text-base text-center sm:text-lg text-neutral-800 dark:text-neutral-200 font-mono">
               Hi! I'm <span className="font-mono">V. Ranadheer</span>, a CS graduate student passionate about the intersection of <span className="font-medium">design</span> and <span className="font-medium">development</span>. Every line of code I write and every pixel I design reflects my commitment to creative problem‑solving. I'm on a journey to discover what it truly means to be a developer — crafting innovative solutions one step at a time, while continuously striving to think and solve problems at a higher level.
             </p>
           </div>
